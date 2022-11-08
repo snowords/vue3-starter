ee3b497a24e459a1a67cb24fa3af6c8351c62610
张延航 <1430750200@qq.com>
2022-11-08 17:29:30 +0800

    feat: add js demo

--- src/pages/tsdemo.vue
+++ src/pages/tsdemo.vue
@@ -14,10 +14,21 @@ import { runTsDemo } from '../utils/tsdemo'
 // };
 
 const tsMsg = runTsDemo()
+
+function sum(num: number) {
+  // your code here
+  const fn = (num2: number) => sum(num + num2);
+  fn.valueOf = () => num;
+  return fn;
+}
+
+const result = sum(1)
 </script>
 <template>
   <div class="w-1/2 h-screen m-auto h-full p-6 bg-gray-600 text-gray-100">
     ...
     <p>{{ tsMsg }}</p>
+    <p>结果：</p>
+    <p>{{ result(2)(3)(5).valueOf() }}</p>
   </div>
 </template>
